350acf288bdd0e39dd5e2369cec44588d6b2e003
unknown <maximiliano.ovejak@gmail.com>
2022-12-21 17:11:38 -0500

    cambios interesantes

--- src/components/MainMenuSection/MainMenuSection.jsx
+++ src/components/MainMenuSection/MainMenuSection.jsx
@@ -1,22 +1,27 @@
 
-// import { useState } from 'react'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
-// import { MenuList } from '..'
+import { MenuList } from '..'
 
 import './MainMenuSection.css'
 
 const MainMenuSection = ({idioma, data}) => {
-  // const [datos, setDatos] = useState([])
+  const [datos, setDatos] = useState([])
   let {id}= useParams()
-  // setDatos()
+  if(id==="mixologia" && id==="postres"){
+    setDatos(data.id)
+  } else{
+    setDatos(data.menu.id)
+  }
+
   return (
     <section className='MainMenuSection'>
         <h1>{id}</h1>
-        {/* {datos && datos.map((e,i)=>{
+        {datos && datos.map((e,i)=>{
             return(
                 <MenuList idioma={idioma} data={e} key={i}/>
             )
-        })} */}
+        })}
     </section>
   )
 }
