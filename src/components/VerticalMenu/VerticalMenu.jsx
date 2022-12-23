ac489ae6e32813f785b420d38c59a913e6bd3a37
unknown <maximiliano.ovejak@gmail.com>
2022-12-23 18:51:56 -0500

    Arreglo de redirección verticalNav

--- src/components/VerticalMenu/VerticalMenu.jsx
+++ src/components/VerticalMenu/VerticalMenu.jsx
@@ -5,11 +5,11 @@ const VerticalMenu = ({showVert,idioma, handleMenuClick}) => {
   return (
   <div className={`VerticalMenu  ${showVert? "Show-Vert" : "Hide-Vert"}`}>
       <div className="slide-in-blurred-right">
-        <NavLink activeClassName="estamos" to="/menu"  onClick={handleMenuClick}>{idioma==="es"?"Menú":"Menu"}</NavLink>
-        <NavLink activeClassName="estamos" to="/mixology" onClick={handleMenuClick}>
+        <NavLink activeClassName="estamos" to="/"  onClick={handleMenuClick}>{idioma==="es"?"Menú":"Menu"}</NavLink>
+        <NavLink activeClassName="estamos" to="/menu/mixology" onClick={handleMenuClick}>
             {idioma==="es"?"Mixología":"Mixology"}
         </NavLink>
-        <NavLink activeClassName="estamos" to="/postres" onClick={handleMenuClick}>
+        <NavLink activeClassName="estamos" to="/menu/postres" onClick={handleMenuClick}>
             {idioma==="es"?"Postres":"Desserts"}
         </NavLink>
         <a href="https://fredshouserestaurant.com/menudigital/destilados.pdf" onClick={handleMenuClick}>{idioma==="es"?"Destilados":"Spirits"} </a>
